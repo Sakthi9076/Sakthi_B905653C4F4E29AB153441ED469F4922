16311af816b95f02dd0075d770f5e9eda6c8544a
Sakthi9076 <144669377+Sakthi9076@users.noreply.github.com>
2023-09-22 12:28:21 +0530

    Add files via upload

---
+++ main (2) (1).py
@@ -0,0 +1,47 @@
+# Leap year# leap year
+
+enter a year: 2020 2020 is a leap year.
+
+2
+
+3 def isleapyear(year):
+
+4
+
+5
+
+i
+
+if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+
+get enthused...
+
+return true
+
+6 else:
+
+7
+
+return false
+
+8
+
+neole
+
+dock
+
+8
+
+ecreta
+
+9 year = int(input("enter a year: "))
+
+10
+
+11 if isleapyear(year):
+
+12 print('{} is a leap year.'.format(year))
+
+13 else:
+
+14 print('{} is not a leap year.'.format(year))￼enter
